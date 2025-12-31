dab9deb7b8c8379c848d21d0aec59e6ee1bd6ef4
taeik <kti1452@naver.com>
2026-01-01 03:05:07 +0900

    chore: setup project environment and install libraries

---
+++ next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
