76ef454d0ee6c4ced481e3dd29820fdc2d5a7ce8
taeik <kti1452@naver.com>
2026-01-01 03:51:16 +0900

    feat: implement main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { About } from '@/components/about';
 import { Contact } from '@/components/contact';
+import { Footer } from '@/components/footer';
 import { Header } from '@/components/header';
 import { Info } from '@/components/info';
 import { Project } from '@/components/project';
@@ -9,11 +10,13 @@ export default function Page() {
   return (
     <>
       <Header />
-      <About />
-      <Info />
-      <Skill />
-      <Project />
-      <Contact />
+      <main className="container mx-auto max-w-4xl px-8">
+        <About />
+        <Info />
+        <Skill />
+        <Project />
+        <Contact />
+      </main>
     </>
   );
 }
